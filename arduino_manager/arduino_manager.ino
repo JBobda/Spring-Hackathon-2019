f7fca86500e55c6f0c0915cd6953a643e294e386
Elijah Helf <Elijah.Helf@jbhunt.com>
2019-03-02 00:03:31 -0600

    Ard update

--- arduino_manager/arduino_manager.ino
+++ arduino_manager/arduino_manager.ino
@@ -3,10 +3,10 @@
 // August 17, 2014
 // Public Domain
 #include<Wire.h>
-const int MPU_addr=0x68;  // I2C address of the MPU-6050
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
-int16_t previousAcX,previousAcY;
-void setup(){
+const int MPU_addr = 0x68; // I2C address of the MPU-6050
+int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+int16_t previousAcX, previousAcY;
+void setup() {
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x6B);  // PWR_MGMT_1 register
@@ -14,29 +14,30 @@ void setup(){
   Wire.endTransmission(true);
   Serial.begin(9600);
 }
-void loop(){
+void loop() {
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
   AcX = 0;
   previousAcX = AcX;
-  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
-  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
-  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Serial.print("AcX = "); Serial.print(AcX);
-  Serial.print(" | AcY = "); Serial.print(AcY);
-  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  Wire.requestFrom(MPU_addr, 14, true); // request a total of 14 registers
+  AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  GyX = Wire.read() << 8 | Wire.read();
+  GyY = Wire.read() << 8 | Wire.read();
+  GyZ = Wire.read() << 8 | Wire.read();
+  int forward_change = AcX - previousAcX;
   check_collision();
-  Serial.print("\n");
   delay(333);
 }
 
-void check_collision(){
-  int16_t threshold = 10000;
+int check_collision() {
+  int16_t threshold = 9000;
   int16_t forward_change = AcX - previousAcX;
   int16_t sideward_change = AcY - previousAcY;
-  if(forward_change >= threshold || sideward_change >= threshold){
-    Serial.print("CRASH");
+  if (abs(forward_change) >= threshold || abs(sideward_change) >= threshold) {
+    Serial.print("Collision detected \n");
+    return 1;
   }
 }
